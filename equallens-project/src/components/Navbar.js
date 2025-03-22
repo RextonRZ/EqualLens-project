ed813bc005a1fc2c9924e208abd3233b305c3705
RZRexton <ooiruizhe@gmail.com>
2025-03-23 00:13:40 +0800

    Update main page component

--- equallens-project/src/components/Navbar.js
+++ equallens-project/src/components/Navbar.js
@@ -43,7 +43,7 @@ function Navbar() {
               </li>
               <li className='nav-item'>
                 <Link to='/upload-cv' className='nav-links' onClick={closeMobileMenu}>
-                  Upload CV
+                  New job
                 </Link>
               </li>
               <li className='nav-item'>
