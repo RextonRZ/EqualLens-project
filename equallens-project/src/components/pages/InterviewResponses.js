b5b3d6e9d8fe2524877fea683da715fbe6a9d97b
RZRexton <ooiruizhe@gmail.com>
2025-03-30 13:50:22 +0800

    Refine UI interview Responses back button

--- equallens-project/src/components/pages/InterviewResponses.js
+++ equallens-project/src/components/pages/InterviewResponses.js
@@ -586,11 +586,11 @@ const InterviewResponses = () => {
             {showSuccessModal && <SuccessModal />}
             {showConfirmModal && <ConfirmModal />}
 
-            <button className="back-button" onClick={() => navigate(`/dashboard/${jobId}/${candidateId}`)}>
+            <button className="back-button" onClick={() => navigate(-1)}>
                 <svg className="back-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7"></path>
                 </svg>
-                Back to Candidate Profile
+                Back
             </button>
 
             <div className="responses-header">
